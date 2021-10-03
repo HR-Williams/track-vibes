524adb9c6a0b9f395cfe88c01df078914d71b8ab
HR Williams <halrubinwilliams@gmail.com>
2021-10-02 22:27:28 -0700

    useState to get search value, may need event prevent default

--- src/Search.js
+++ src/Search.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Search = () => {
 
-  function handleSearch(event) {
-  event.preventDefault();
-  console.log(event.target.artist.value);
-}
+  const [searchedArtistValue, setSearchedArtistValue] = useState('');
+
+  // function handleSearch(event) {
+  // event.preventDefault();
+  // console.log(event.target.artist.value);
+// }
   return(
     <React.Fragment>
-      <form onSubmit={handleSearch}>
+      {/* <form onSubmit={handleSearch}> */}
+      {/* does this need an event prevent default to not reload after search? */}
+      <form value={searchedArtistValue} onSubmit={e => setSearchedArtistValue(e.target.artist.value)}>
         <input
           type='text'
           name='artist'
           placeholder='Artist' />
           <button type='submit'>Search</button>
       </form>
+      <p>Searched artist: {searchedArtistValue}</p>
     </React.Fragment>
   );
 }
