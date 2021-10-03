e09a4307193f132c841fff82803ee7c6235d77cb
HR Williams <halrubinwilliams@gmail.com>
2021-10-03 11:55:27 -0700

    onSubmit handleSearch gets searched artist and logs properly in useState

--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,27 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-const Search = () => {
+const Search = props => {
 
   const [searchedArtistValue, setSearchedArtistValue] = useState('');
 
+  const handleSearch = event => {
+    event.preventDefault();
+    setSearchedArtistValue(event.target.artist.value);
+  }
   // function handleSearch(event) {
   // event.preventDefault();
   // console.log(event.target.artist.value);
+  
 // }
+console.log("searchedArtistValue " + searchedArtistValue);
+//this console.log above blips correctly then goes away
   return(
     <React.Fragment>
       {/* <form onSubmit={handleSearch}> */}
       {/* does this need an event prevent default to not reload after search? */}
-      <form value={searchedArtistValue} onSubmit={e => setSearchedArtistValue(e.target.artist.value)}>
+      {/* <form value={searchedArtistValue} onSubmit={event => setSearchedArtistValue(event.target.artist.value)}> */}
+      <form value={searchedArtistValue} onSubmit={handleSearch}>
+
         <input
           type='text'
           name='artist'
@@ -20,7 +29,10 @@ const Search = () => {
           <button type='submit'>Search</button>
       </form>
       <p>Searched artist: {searchedArtistValue}</p>
+      <p>artistState: {props.artistState}</p> 
+      
     </React.Fragment>
+    
   );
 }
 
